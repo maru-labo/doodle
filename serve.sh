6a757c1d5bb5c7576a2e10130fb6eeeb0daeb517
Arata Furukawa <info@ornew.net>
2018-04-25 22:21:21 +0900

    rename serving dir

--- serve.sh
+++ serve.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+model_dir=/var/lib/tensorflow-model
+
 docker run -it --rm \
-    -v $(pwd)/export:/var/lib/tensorflow-model \
+    -v $(pwd)/model:$model_dir \
     -p 8500:8500 \
     ornew/tensorflow-serving-api-server \
     tensorflow_model_server \
     --port=8500 \
-    --model_config_file=/var/lib/tensorflow-model/config.pb.txt \
+    --model_config_file=$model_dir/config.pb.txt \
     --enable_batching=true
