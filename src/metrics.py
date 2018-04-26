d816d21999a7698ebad8e0be9cbc7ed737cbefb8
Arata Furukawa <info@ornew.net>
2018-04-26 14:36:23 +0900

    improve human readable

--- src/metrics.py
+++ src/metrics.py
@@ -3,12 +3,12 @@
 """
 モデルの評価指針となるメトリクスを計算するモジュールです。
 
-カテゴリ分類器でよくもちいられるメトリックを計算します。まず以下の4つの統計量を出します。
+分類器でよく用いられるメトリックを計算します。まずクラスごとに4つの統計量を出します。
 
-    True  Ppsitive   : ポジティブな予測が正解だった数。
-    True  Negative   : ネガティブな予測が正解だった数。
-    False Positive   : ポジティブな予測が不正解だった数。
-    False Negative   : ネガティブな予測が不正解だった数。
+    True  Ppsitive   : 正の予測が正解だった数。
+    True  Negative   : 負の予測が正解だった数。
+    False Positive   : 正の予測が不正解だった数。
+    False Negative   : 負の予測が不正解だった数。
 
 メトリクスは以下の4つを計算します。
 
@@ -22,20 +22,19 @@ Accuracyは正解した割合です。わかりやすいので精度として扱
 Accuracyではどちらで間違えているのかがわかりません。
 
 2つの間違いの違いはよく病気の誤診で説明されます。
-    - 「病気じゃないのに病気と診断した(FP)」
-    - 「病気だったのに病気じゃないと診断した(FN)」
-両者は明らかに深刻度が異なります。
-前者は「間違えました」で済まされるかもしれませんが、後者は重い病気であれば大変なミスになります。
+    - FP 「病気じゃないのに病気と診断した」
+    - FN 「病気だったのに病気じゃないと診断した」
+両者は明らかに深刻さが異なります。
+前者は「間違えました」で済まされるかもしれませんが、後者は、もし重い病気であれば命に関わりますね。
 
 そこで、適合率と再現率を見ます。
 
-適合率は、予測が正のもののうち正解も正だった割合なので、上記例であれば「病気だと診断した時に、本当に病気だった割合」です。
-再現率は、正解が正のもののうち予測が正だった割合なので、上記例であれば「病気の人を、病気と予測できた割合」です。
+適合率は、上記例であれば「病気だと診断した時に、本当に病気だった割合」です。
+再現率は、上記例であれば「病気の人を、病気と予測できた割合」です。
 
 病気の患者を漏れなく発見することが重要な場合、再現率が重要となります。
 適合率と再現率はトレードオフになることが多いです。
 両者のどちらが重要なのかは、ケースによります。誤診の例では再現率でしたが、必ずしも再現率が重要とは限りません。
-手書き数字ではぶっちゃけどっちでもあまり関係ありませんが、計算しておきましょう。
 また、両者のバランスをとった指針としてF値というものがあります。
 
 それぞれ、マクロ平均とマイクロ平均を計算します。
@@ -48,9 +47,10 @@ Accuracyではどちらで間違えているのかがわかりません。
 クラスごとに平等に値を計算したい場合は、マクロ平均を用います。
 """
 
+import six
 import tensorflow as tf
 
-def calculate(labels, classes, num_classes):
+def calculate(labels, classes, num_classes, add_summary=True):
     cm = tf.confusion_matrix(labels, classes, num_classes=num_classes, dtype=tf.float32)
     ln = tf.reduce_sum(cm)
     tp = tf.diag_part(cm)
@@ -58,13 +58,16 @@ def calculate(labels, classes, num_classes):
     fn = tf.reduce_sum(cm, axis=0) - tp
     tn = ln - tp - fp - fn
     eps = tf.convert_to_tensor(1e-7)
-    _micros = micro_metrics(tp, fp, tn, fn, eps)
-    _macros = macro_metrics(tp, fp, tn, fn, num_classes, eps)
-
-    return {
-        **_micros,
-        **_macros,
+    metric_ops = {
+        **micro_metrics(tp, fp, tn, fn, eps),
+        **macro_metrics(tp, fp, tn, fn, num_classes, eps),
     }
+
+    if add_summary:
+        for name, metric in six.iteritems(metric_ops):
+            tf.summary.scalar(name, metric[1])
+
+    return metric_ops
 
 def micro_metrics(tp, fp, tn, fn, eps):
     with tf.name_scope('micro_average'):
