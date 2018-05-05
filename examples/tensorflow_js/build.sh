f3527116cdcd1203ebc64220241fa367035a4b9d
ornew <info@ornew.net>
2018-05-06 07:50:57 +0900

    update build.sj

--- examples/tensorflow_js/build.sh
+++ examples/tensorflow_js/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pushd dist
-tar cvzf ../tensorflowjs.doodle.marulabo.tar.gz index.html buefy.min.*.css marulabo.*.svg src.*.js
+tar cvzf ../tensorflowjs.doodle.marulabo.tar.gz index.html buefy.min.css index.js
 popd
 
